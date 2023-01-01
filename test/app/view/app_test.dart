ab384b2b32797f44967f187d7fe323c225a86991
thaihuynhxyz <thaihuynhxyz@gmail.com>
2023-01-01 10:49:42 +0700

    Fix analyze

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_app/app/app.dart';
 import 'package:flutter_app/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('App', () {
